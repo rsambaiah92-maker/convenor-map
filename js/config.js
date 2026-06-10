343cceba47bae23913e760cad0484d64eb366fbe
rsambaiah92-maker <rsambaiah92@gmail.com>
2026-06-10 18:18:41 +0530

    Update Supabase ANON KEY in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://urxdqjndesznqinwnfvw.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeGRxam5kZXN6bnFpbnduZnZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwNjg5OTMsImV4cCI6MjA5NjY0NDk5M30.zqI76MGXXmQIQ_Y9fSLdZPFrVsgolVx_cQDwu2bsqsc";
+const SUPABASE_ANON_KEY = "sb_publishable_OOSr0qknjUgqJdzQ-4q_dw_-Cf1nGQv";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
