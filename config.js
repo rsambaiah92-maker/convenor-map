32409849596ed6672fb5ee1b34b3e17f4c201b14
rsambaiah92-maker <rsambaiah92@gmail.com>
2026-06-10 16:51:35 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://urxdqjndesznqinwnfvw.supabase.co";
-
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY_HERE";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
