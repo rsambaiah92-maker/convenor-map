5e847a7f150af1acdf09af19b4925ae0f32ecc4e
rsambaiah92-maker <rsambaiah92@gmail.com>
2026-06-18 23:04:23 +0530

    Update service worker cache name and URLs

--- sw.js
+++ sw.js
@@ -1,23 +1,27 @@
-const CACHE_NAME = "tgnpdcl-v1";
-
+const CACHE_NAME = 'tgnpdcl-dashboard-v2';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./css/style.css",
-  "./images/icon-192.png",
-  "./images/icon-512.png"
+  './index.html',
+  './user.html',
+  './css/style.css',
+  './js/config.js',
+  './js/auth.js',
+  './manifest.json',
+  './images/icon-192.png',
+  './images/icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
